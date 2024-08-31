fd72dfe7160d322a950cbb500ea54b0550d6579f
SebastianLozanoValle <cesarloco2003@hotmail.com>
2024-08-30 21:55:18 -0500

    ....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import RootLayout from "./layout";
 
-export default function Home(): JSX.Element {
+export default function home(): JSX.Element {
   return (
     <RootLayout>
       <h1>Welcome to the Inlaze Next.js Starter!</h1>
